41b8f8790a0cd9c7ebaefd0c127b7011aa00bd5a
GonzaloArmoa <gonzalo-armoa@hotmail.com>
2023-05-22 20:21:52 -0300

    cambios en la ensaladera

--- ensaladera.js
+++ ensaladera.js
@@ -2,9 +2,10 @@ console.log('Aquí esta la ensaladera')
 console.log('Instrucciones')
 console.log(`
 1. Lavar las verduras
-2. Cortar a mano la lechuga
-3. Cortar el tomato en rodajas, a 45 grados
-4. Cortar la cebolla a la juliana
-5. Cortar en tiritas el jamon y el queso
+2. Cortar la lechuga a cuchillo
+3. Cortar el tomato en cubitos
+4. Cortar la cebolla rodajas finas
+5. Cortar en cubitos el jamon y el queso
 6. Añadir el huevo en trozos pequeños
-7. La ensalada queda finalizada`)
+7. Condimentar a gusto.
+8. La ensalada queda finalizada`)
